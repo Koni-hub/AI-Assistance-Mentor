33f941bf30935c41ac88c38c1bbce1b776417520
koni-hub <delgadoargie04z@gmail.com>
2024-11-25 22:11:33 +0800

    chore: add part model

---
+++ app/src/main/java/com/ai/mentor_assistant/Models/Part.java
@@ -0,0 +1,15 @@
+package com.ai.mentor_assistant.Models;
+
+// Part Model
+public class Part {
+    private String text;
+
+    public Part(String text) {
+        this.text = text;
+    }
+
+    // Getter
+    public String getText() {
+        return text;
+    }
+}
